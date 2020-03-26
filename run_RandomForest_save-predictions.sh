797047ecdc0cf8a475acec7c242259c055dd3f5b
Tushar Goswamy <39840108+tgoswamy@users.noreply.github.com>
2020-03-27 03:27:13 +0530

    Add files via upload

--- run_RandomForest_save-predictions.sh
+++ run_RandomForest_save-predictions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # TODO CHANGE PATH ACCORDINGT TO YOUR MACHINE!
-export CLASSPATH=.....weka-3-9-1/weka.jar
+export CLASSPATH=/home/tgoswamy/weka/weka/dist/weka.jar
 echo "Train Folder: $1"
 echo "Test Input: $2"
 ## This executes weka + saves the output to a CSV file including Tweet ID and tweet (String)
